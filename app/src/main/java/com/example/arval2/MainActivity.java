1f76b7fc1405f7a8f629810c1eb155d1bb592950
papichou <valovaly6@gmail.com>
2019-06-05 16:44:42 +0200

    appli AR avec modele d'un pot de fleur

---
+++ app/src/main/java/com/example/arval2/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.arval2;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+    private ModelRenderable mon_modele;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("01Alocasia_obj.sfb"))
+                .build()
+                .thenAccept(renderable -> mon_modele = renderable);
+
+
+        arFragment.setOnTapArPlaneListener(
+                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                    if (mon_modele == null) {
+                        return;
+                    }
+                    if (plane.getType() != Plane.Type.HORIZONTAL_UPWARD_FACING) {
+                        return;
+                    }
+
+
+                    //On créé le point d'encrage du modèle 3d
+                    Anchor anchor = hitResult.createAnchor();
+                    AnchorNode anchorNode = new AnchorNode(anchor);
+                    anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                    //On attache ensuite notre modèle au point d'encrage
+                    TransformableNode Node = new TransformableNode(arFragment.getTransformationSystem());
+                    Node.setParent(anchorNode);
+                    Node.setRenderable(mon_modele);
+                    Node.select();
+
+
+                }
+        );
+    }
+}
